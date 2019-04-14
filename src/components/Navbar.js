3ca76f142420e2e2359ffec7046fa12c4b5de091
Westley Feller <feller.westley@gmail.com>
2019-04-13 20:23:02 -0700

    Removed product
    There are no products, therefore, no pages

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "gatsby";
+import {Link} from "gatsby";
 import github from "../img/github-icon.svg";
 import logo from "../img/logo.svg";
 
@@ -63,17 +63,11 @@ const Navbar = class extends React.Component {
               <Link className="navbar-item" to="/about">
                 About
               </Link>
-              <Link className="navbar-item" to="/products">
-                Products
-              </Link>
               <Link className="navbar-item" to="/blog">
                 Blog
               </Link>
               <Link className="navbar-item" to="/contact">
                 Contact
-              </Link>
-              <Link className="navbar-item" to="/contact/examples">
-                Form Examples
               </Link>
             </div>
             <div className="navbar-end has-text-centered">
