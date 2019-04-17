b9a60b7ba8d1bd59b8e592915d7347ca0a9b2481
Westley Feller <feller.westley@gmail.com>
2019-04-16 18:44:47 -0700

    Registered trip preview with cms

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -2,8 +2,10 @@ import CMS from 'netlify-cms'
 
 import AboutPagePreview from './preview-templates/AboutPagePreview'
 import BlogPostPreview from './preview-templates/BlogPostPreview'
+import TripPostPreview from './preview-templates/TripPostPreview'
 import IndexPagePreview from './preview-templates/IndexPagePreview'
 
 CMS.registerPreviewTemplate('index', IndexPagePreview);
 CMS.registerPreviewTemplate('about', AboutPagePreview);
 CMS.registerPreviewTemplate('blog', BlogPostPreview);
+CMS.registerPreviewTemplate('trip', TripPostPreview);
