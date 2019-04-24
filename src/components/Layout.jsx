b4093ddd7fca32c612b5919bd2b0225ea755f00c
Westley Feller <feller.westley@gmail.com>
2019-04-23 18:00:38 -0700

    Removed sass packages

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import Helmet from 'react-helmet'
 import Footer from './Footer'
 import Navbar from './Navbar'
-import './all.sass'
 import useSiteMetadata from './SiteMetadata'
 
 const TemplateWrapper = ({children}) => {
