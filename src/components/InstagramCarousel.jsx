cb722b17f4c7816b8fa0326902735d0a138992d8
Westley Feller <feller.westley@gmail.com>
2019-05-10 17:41:50 -0700

    Instagram to class component

--- src/components/InstagramCarousel.jsx
+++ src/components/InstagramCarousel.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 import Slider from 'react-slick';
 import Img from "gatsby-image";
@@ -7,83 +7,85 @@ import "slick-carousel/slick/slick-theme.css"
 
 import {useStaticQuery, graphql} from 'gatsby'
 
-function InstagramCarousel() {
-    const {allFile} = useStaticQuery(
-        graphql`
-            query {
-                allFile(
-                    sort: { fields: name, order: DESC }
-                    filter: { sourceInstanceName: { eq: "insta" } }
-                ) {
-                    edges {
-                        node {
-                            id
-                            name
-                            childImageSharp {
-                                fluid(maxWidth: 2080, maxHeight: 2080) {
-                                    ...GatsbyImageSharpFluid_withWebp_tracedSVG
+class InstagramCarousel extends Component {
+    render() {
+        const {allFile} = useStaticQuery(
+            graphql`
+                query {
+                    allFile(
+                        sort: { fields: name, order: DESC }
+                        filter: { sourceInstanceName: { eq: "insta" } }
+                    ) {
+                        edges {
+                            node {
+                                id
+                                name
+                                childImageSharp {
+                                    fluid(maxWidth: 2080, maxHeight: 2080) {
+                                        ...GatsbyImageSharpFluid_withWebp_tracedSVG
+                                    }
                                 }
                             }
                         }
                     }
                 }
-            }
-        `
-    );
+            `
+        );
 
-    let images = [];
+        let images = [];
 
-    allFile.edges.forEach(({node}) => {
-        images.push(<div key={node.id}>
-            <Img fluid={node.childImageSharp.fluid}
-                 alt={node.name.replace(/-/g, ' ')}
-                 backgroundColor={`#A1B5B2`}
-            />
-        </div>);
-    });
+        allFile.edges.forEach(({node}) => {
+            images.push(<div key={node.id}>
+                <Img fluid={node.childImageSharp.fluid}
+                     alt={node.name.replace(/-/g, ' ')}
+                     backgroundColor={`#A1B5B2`}
+                />
+            </div>);
+        });
 
-    const settings = {
-        dots: false,
-        arrows: false,
-        infinite: true,
-        speed: 500,
-        slidesToShow: 8,
-        slidesToScroll: 8,
-        autoplay: true,
-        autoplaySpeed: 4000,
-        pauseOnFocus: true,
-        pauseOnHover: true,
-        lazyLoad: 'progressive',
-        responsive: [
-            {
-                breakpoint: 1920,
-                settings: {
-                    slidesToShow: 6,
-                    slidesToScroll: 6,
+        const settings = {
+            dots: false,
+            arrows: false,
+            infinite: true,
+            speed: 500,
+            slidesToShow: 8,
+            slidesToScroll: 8,
+            autoplay: true,
+            autoplaySpeed: 4000,
+            pauseOnFocus: true,
+            pauseOnHover: true,
+            lazyLoad: 'progressive',
+            responsive: [
+                {
+                    breakpoint: 1920,
+                    settings: {
+                        slidesToShow: 6,
+                        slidesToScroll: 6,
+                    }
+                },
+                {
+                    breakpoint: 1280,
+                    settings: {
+                        slidesToShow: 4,
+                        slidesToScroll: 4
+                    }
+                },
+                {
+                    breakpoint: 640,
+                    settings: {
+                        slidesToShow: 2,
+                        slidesToScroll: 2
+                    }
                 }
-            },
-            {
-                breakpoint: 1280,
-                settings: {
-                    slidesToShow: 4,
-                    slidesToScroll: 4
-                }
-            },
-            {
-                breakpoint: 640,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 2
-                }
-            }
-        ]
-    };
+            ]
+        };
 
-    return (
-        <Slider {...settings}>
-            {images}
-        </Slider>
-    )
+        return (
+            <Slider {...settings}>
+                {images}
+            </Slider>
+        )
+    }
 }
 
 export default InstagramCarousel
