0f5152daacf347760d449663d3db6d96666034fd
Westley Feller <feller.westley@gmail.com>
2019-05-10 17:21:33 -0700

    Moved insta to src

--- gatsby-config.js
+++ gatsby-config.js
@@ -65,7 +65,7 @@ module.exports = {
             // keep as first gatsby-source-filesystem plugin for gatsby image support
             resolve: 'gatsby-source-filesystem',
             options: {
-                path: `${__dirname}/static/img/insta`,
+                path: `${__dirname}/src/img/insta`,
                 name: 'insta',
             },
         },
